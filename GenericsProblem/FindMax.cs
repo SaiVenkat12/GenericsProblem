ae9ee38f4bd3eb24e9ff0ade80dbffcd39aea451
SaiVenkat12 <gsaiv1001@gmail.com>
2023-02-10 21:35:51 +0530

    [Sai Venkat]-Added Greatest among 3 Numbers

---
+++ GenericsProblem/FindMax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    internal class FindMax
+    {
+        public int FindMaxNumber(int first,int second,int third)
+        {
+            
+            if(first.CompareTo(second)>0 && first.CompareTo(third)>0)
+            {
+                return first;
+            }
+            else if(second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+               return second;
+            }
+            else if(third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            throw new Exception("2 or 3 Values are Equal");
+        }
+    }
+}
---
+++ GenericsProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generics Problem");
+            Console.WriteLine("Max Value");
+            FindMax maxValue= new FindMax();
+            Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(21, 14, 15));
+            Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(12, 90, 15));
+            Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(12, 14, 25));
+        }
+    }
+}
