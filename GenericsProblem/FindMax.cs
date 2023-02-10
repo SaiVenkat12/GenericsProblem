2ae230191cd71d06b653dfd4456b8258a4741787
SaiVenkat12 <gsaiv1001@gmail.com>
2023-02-10 21:44:23 +0530

    [Sai Venkat]-Added Greatest Float Value

--- GenericsProblem/FindMax.cs
+++ GenericsProblem/FindMax.cs
@@ -25,5 +25,22 @@ namespace GenericsProblem
             }
             throw new Exception("2 or 3 Values are Equal");
         }
+        public float FindMaxFloat(float first, float second, float third)
+        {
+
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            throw new Exception("2 or 3 Values are Equal");
+        }
     }
 }
--- GenericsProblem/Program.cs
+++ GenericsProblem/Program.cs
@@ -13,9 +13,26 @@ namespace GenericsProblem
             Console.WriteLine("Generics Problem");
             Console.WriteLine("Max Value");
             FindMax maxValue= new FindMax();
-            Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(21, 14, 15));
-            Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(12, 90, 15));
-            Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(12, 14, 25));
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Find Max Number \n2.Find Max Float \n22.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(21, 14, 15));
+                        Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(12, 90, 15));
+                        Console.WriteLine("The Greatest Number is {0}", maxValue.FindMaxNumber(12, 14, 25));
+                        break;
+                    case 2:
+                        Console.WriteLine("The Greatest Value is {0}", maxValue.FindMaxFloat(21.5f, 14.9f, 15));
+                        Console.WriteLine("The Greatest Value is {0}", maxValue.FindMaxFloat(12, 90, 15.3f));
+                        Console.WriteLine("The Greatest Value is {0}", maxValue.FindMaxFloat(12, 14, 25.1f));
+                        break;
+                }
+            }
+                        
         }
     }
 }
